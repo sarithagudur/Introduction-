888051164cd630fb07e95993509cd800fed50b90
sarithagudur <42064481+sarithagudur@users.noreply.github.com>
2025-09-14 12:16:11 +0100

    Add files via upload

---
+++ type_casting.py
@@ -0,0 +1,14 @@
+number1=5
+number2=5.5
+result=True
+name="Aadhya"
+
+print(type(number1))
+print(type(number2))
+print(type(result))
+print(type(name))
+
+number3=int(number2)
+print(number3)
+number4=float(number1)
+print(number4)
